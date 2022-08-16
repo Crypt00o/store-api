4685b3eb05b906806deae5f93fb7b4a46ea32a67
Crypt00o <octopus2672001@gmail.com>
2022-08-16 13:16:06 +0200

    Add User Model Specs for Testing

--- src/utils/hashing.ts
+++ src/utils/hashing.ts
@@ -1,4 +1,4 @@
-import {hashSync,compareSync} from "bcrypt"
+import {hashSync} from "bcrypt"
 import {SALT_ROUND,BCRYPT_PEPPER} from "../config"
 const hashingPassword=(password:string):string=>{
 const password_plus_pepper=password.concat(BCRYPT_PEPPER as string)
